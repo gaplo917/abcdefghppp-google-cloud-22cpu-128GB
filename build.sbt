b907e1ca06d5fbdd3598646c3f78f17614ac7cae
Gary Lo <logap@me.com>
2016-04-02 00:54:07 +0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "abcdefghppp-google-cloud-22cpu-128GB"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+javaOptions += "-Xms2G -Xmx2G"
---
+++ src/main/scala/Main/Main.scala
@@ -0,0 +1,207 @@
+package Main
+
+import scala.collection.parallel.ParSeq
+
+/**
+  * Created by Gaplo917 on 2/4/2016.
+  */
+object Main {
+
+  val BASE = 17
+
+  val WIDTH = 4
+  //  val WIDTH = 4
+
+  val baseAnd1 = BASE + 1
+  val possibleToTry = (0 until BASE).toVector.filter(x => x != 1)
+
+  def intToChar(x:Int) = if( x > 9) (x - 10 + 'a'.toInt).toChar else (x - 1 + '1'.toInt).toChar
+
+  object BranchState extends Enumeration {
+    type BranchState = Value
+    val Initial,State_1, State_11,State_0,State_10 = Value
+  }
+
+  import BranchState._
+
+  def leadingDigitPossibleSolution(possibleToTry: Vector[Int], result:Int, lastLending:Int) = {
+    possiblePermuatationN(possibleToTry,3)
+      .filter {
+        case xs@Vector(a,c,g) =>
+          val na = a - lastLending
+          val e = result - g
+          na > 1 && na > c && c > 1 && g > 1 && na - c + g == result && e != 1 &&  !xs.contains(e)
+      }
+      .map{
+        case xs@Vector(a,c,g) =>
+          Vector(a, c, result - g, g)
+      }.toStream.par
+  }
+
+  def innerDigitPossibleSolution(possibleToTry: Vector[Int], result:Int, lending:Int, lastLending:Int) = {
+    possiblePermuatationN(possibleToTry,3)
+      .filter {
+        case xs@Vector(b,d,h) =>
+          val f = result - h
+          f != 1 && f >= 0  && f < BASE && (b + lending * BASE) - lastLending - d + h == result && f < BASE && !xs.contains(f)
+      }
+      .map{
+        case xs@Vector(b,d,h) =>
+          Vector(b, d, result - h, h)
+      }.toStream.par
+  }
+
+
+  val InnerDigitPossibleSolutionMap  =
+    Map(
+      (State_11,1,1) -> innerDigitPossibleSolution(possibleToTry,baseAnd1,1,1),
+      (State_11,1,0) -> innerDigitPossibleSolution(possibleToTry,baseAnd1,1,0),
+      (State_11,0,1) -> innerDigitPossibleSolution(possibleToTry,baseAnd1,0,1),
+      (State_11,0,0) -> innerDigitPossibleSolution(possibleToTry,baseAnd1,0,0),
+      (State_10,1,1) -> innerDigitPossibleSolution(possibleToTry,BASE,1,1),
+      (State_10,1,0) -> innerDigitPossibleSolution(possibleToTry,BASE,1,0),
+      (State_10,0,1) -> innerDigitPossibleSolution(possibleToTry,BASE,0,1),
+      (State_10,0,0) -> innerDigitPossibleSolution(possibleToTry,BASE,0,0)
+    )
+
+
+  val leadingDigitPossibleSolutionMap =
+    Map(
+      (State_11,1) -> leadingDigitPossibleSolution(possibleToTry,baseAnd1,1),
+      (State_11,0) -> leadingDigitPossibleSolution(possibleToTry,baseAnd1,0),
+      (State_10,1) -> leadingDigitPossibleSolution(possibleToTry,BASE,1),
+      (State_10,0) -> leadingDigitPossibleSolution(possibleToTry,BASE,0)
+    )
+
+  def mainRecur() = {
+    val a = func(
+      possibleToTry = possibleToTry,
+      branchState = State_11,
+      lending = 1
+    )
+
+    val c = func(
+      possibleToTry = possibleToTry,
+      branchState = State_11,
+      lending = 0
+    )
+
+    a ++ c
+  }
+
+  def func(possibleToTry: Vector[Int], branchState: BranchState = Initial, pos: Int = 1, lending:Int = 0, lastLending: Int = 0, possibleSolutions: ParSeq[Vector[Int]] = ParSeq()):  ParSeq[Vector[Int]] = {
+
+    if(pos == WIDTH) {
+      val leadingDigitPossibleSolutions = leadingDigitPossibleSolutionMap(branchState,lastLending)
+
+      if(pos > 1) leadingDigitPossibleSolutions.flatMap {
+        case ps => possibleSolutions.filter(xs => !xs.contains(ps(0)) && !xs.contains(ps(1)) && !xs.contains(ps(2)) && !xs.contains(ps(3)) ).map(ps ++ _)
+      } else possibleSolutions
+
+    } else {
+
+      val innerDigitPossibleSolutions = InnerDigitPossibleSolutionMap(branchState,lending,lastLending)
+
+      val newPossibleSolutions = if(pos > 1) InnerDigitPossibleSolutionMap(branchState,lending,lastLending).flatMap {
+        case ps => possibleSolutions.filter(xs => !xs.contains(ps(0)) && !xs.contains(ps(1)) && !xs.contains(ps(2)) && !xs.contains(ps(3)) ).map(ps ++ _)
+      } else innerDigitPossibleSolutions
+
+      branchState match {
+        case State_1 | State_0 =>
+          // due to the fact that 1 - h > 0
+          ParSeq()
+
+        case state@ State_11 =>
+
+          if(pos + 1 == WIDTH) {
+            // no landing is allows if that is already the leading digit
+            func(
+              possibleToTry = possibleToTry,
+              branchState = State_10,
+              lending = 0,
+              lastLending = lending,
+              pos = pos + 1,
+              possibleSolutions = newPossibleSolutions
+            )
+          } else {
+            func(
+              possibleToTry = possibleToTry,
+              branchState = State_10,
+              lending = 0,
+              lastLending = lending,
+              pos = pos + 1,
+              possibleSolutions = newPossibleSolutions
+            ) ++
+              func(
+                possibleToTry = possibleToTry,
+                branchState = State_10,
+                lending = 1,
+                lastLending = lending,
+                pos = pos + 1,
+                possibleSolutions = newPossibleSolutions
+              )
+          }
+
+        case state@ State_10 =>
+          if(pos + 1 == WIDTH) {
+            // no landing is allows if that is already the leading digit
+            func(
+              possibleToTry = possibleToTry,
+              branchState = State_10,
+              lending = 0,
+              lastLending = lending,
+              pos = pos + 1,
+              possibleSolutions = newPossibleSolutions
+            )
+          } else {
+            func(
+              possibleToTry = possibleToTry,
+              branchState = State_10,
+              lending = 0,
+              lastLending = lending,
+              pos = pos + 1,
+              possibleSolutions = newPossibleSolutions
+            ) ++
+              func(
+                possibleToTry = possibleToTry,
+                branchState = State_10,
+                lending = 1,
+                lastLending = lending,
+                pos = pos + 1,
+                possibleSolutions = newPossibleSolutions
+              )
+          }
+
+      }
+
+    }
+
+  }
+
+  def possiblePermuatationN(xs: Vector[Int], n: Int) = {
+    xs.combinations(n)
+      .flatMap(_.permutations)
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val start = System.currentTimeMillis()
+    println("Start Calculation")
+
+    val solutions = mainRecur()
+
+    println(s"Total number of solutions = ${solutions.size}")
+
+    println(s"Total Time used to solve AB - CD = 111 - GH = EF (Base $BASE): ${System.currentTimeMillis() - start}ms")
+
+    solutions
+      .take(50)
+      .map(xs => xs.map(intToChar))
+      .foreach {
+        case xs@Vector(a,c,e,g,b,d,f,h) =>
+          println(s"$a$b - $c$d = $e$f, $e$f + $g$h = 111")
+        case xs@Vector(a,e,i,m,b,f,j,n,c,g,k,o,d,h,l,p) =>
+          println(s"$a$b$c$d - $e$f$g$h = $i$j$k$l, $i$j$k$l + $m$n$o$p = 11111")
+      }
+  }
+}
